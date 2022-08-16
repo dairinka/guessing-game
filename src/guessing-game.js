0fc8f20229c54c2b5bf4df14ca17f53bf4c55aca
dairinka <104695300+dairinka@users.noreply.github.com>
2022-08-16 22:54:07 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.middleRange = null;
+    }
 
-    setRange(min, max) {
-
+   setRange(min, max) {
+    this.min = min;
+    this.max = max;
     }
 
     guess() {
-
+        console.log('this.max: ', this.max)
+        console.log('this.min: ', this.min)
+     
+      this.middleRange = (Math.round((this.max - this.min) / 2)) + this.min;
+      console.log('this.middleRange: ', this.middleRange)
+      return this.middleRange;
     }
 
     lower() {
-
+        this.max = this.middleRange;
+        return this.guess();
     }
 
     greater() {
-
+        this.min = this.middleRange;
+        return this.guess();
     }
 }
 
